d625d00cb352ceb2312cae1afe388b44b3b56dbc
Lars Hanisch <lars.hanisch@b-soft.de>
2024-03-04 09:10:56 +0100

    feat(todo) - refactor store methods declaration

--- src/app/todo/todo.store.ts
+++ src/app/todo/todo.store.ts
@@ -1,6 +1,7 @@
-import { computed, inject } from '@angular/core';
+import { Signal, computed, inject } from '@angular/core';
 
 import {
+  StateSignal,
   patchState,
   signalStore,
   withComputed,
@@ -48,8 +49,70 @@ const initialState: TodoState = {
   addError: null,
 };
 
+const searchTodos = (
+  store: StateSignal<TodoState>,
+  todoService: TodoService
+) => {
+  return rxMethod<SearchTodoRequest>(
+    pipe(
+      tap(() => patchState(store, { searchBusy: true })),
+      switchMap((request) =>
+        todoService.search(request).pipe(
+          map(
+            (searchResponse): Partial<TodoState> => ({
+              searchResponse,
+              searchError: null,
+            })
+          ),
+          catchError((err) =>
+            of({
+              searchResponse: null,
+              searchError: `${err}`,
+            } satisfies Partial<TodoState>)
+          )
+        )
+      ),
+      tap((state) => {
+        patchState(store, { ...state, searchBusy: false });
+      })
+    )
+  );
+};
+
+const connectAddTodo = (
+  store: StateSignal<TodoState>,
+  searchRequest: Signal<SearchTodoRequest>,
+  todoService: TodoService
+) => {
+  return rxMethod<AddTodoRequest>(
+    pipe(
+      tap(() => patchState(store, { addBusy: true })),
+      switchMap((request) =>
+        todoService.add(request).pipe(
+          map(
+            (addResponse): Partial<TodoState> => ({
+              searchRequest: { ...searchRequest() },
+              addResponse,
+              addError: null,
+            })
+          ),
+          catchError((err) =>
+            of({
+              addResponse: null,
+              addError: `${err}`,
+            } satisfies Partial<TodoState>)
+          )
+        )
+      ),
+      tap((state) => {
+        patchState(store, { ...state, addBusy: false });
+      })
+    )
+  );
+};
+
 export const TodoStore = signalStore(
-  withState(initialState),
+  withState<TodoState>(initialState),
   withComputed((store) => ({
     todos: computed(() => store.searchResponse()?.todos ?? []),
     busy: computed(() => store.searchBusy() || store.addBusy()),
@@ -58,55 +121,8 @@ export const TodoStore = signalStore(
     const todoService = inject(TodoService);
 
     return {
-      searchTodos: rxMethod<SearchTodoRequest>(
-        pipe(
-          tap(() => patchState(store, { searchBusy: true })),
-          switchMap((request) =>
-            todoService.search(request).pipe(
-              map(
-                (searchResponse): Partial<TodoState> => ({
-                  searchResponse,
-                  searchError: null,
-                })
-              ),
-              catchError((err) =>
-                of({
-                  searchResponse: null,
-                  searchError: `${err}`,
-                } satisfies Partial<TodoState>)
-              )
-            )
-          ),
-          tap((state) => {
-            patchState(store, { ...state, searchBusy: false });
-          })
-        )
-      ),
-      connectAddTodo: rxMethod<AddTodoRequest>(
-        pipe(
-          tap(() => patchState(store, { addBusy: true })),
-          switchMap((request) =>
-            todoService.add(request).pipe(
-              map(
-                (addResponse): Partial<TodoState> => ({
-                  searchRequest: { ...store.searchRequest() },
-                  addResponse,
-                  addError: null,
-                })
-              ),
-              catchError((err) =>
-                of({
-                  addResponse: null,
-                  addError: `${err}`,
-                } satisfies Partial<TodoState>)
-              )
-            )
-          ),
-          tap((state) => {
-            patchState(store, { ...state, addBusy: false });
-          })
-        )
-      ),
+      searchTodos: searchTodos(store, todoService),
+      connectAddTodo: connectAddTodo(store, store.searchRequest, todoService),
     };
   }),
   withHooks({
